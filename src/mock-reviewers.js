09d5a5d363bda66a2e86317e58dd35713e3a3ed2
Krzysztof Prońko <pronko.krzysztof@gmail.com>
2022-09-25 19:59:26 +0200

    added reviewers mock data

---
+++ src/mock-reviewers.js
@@ -0,0 +1,41 @@
+
+
+// local reviews data
+export const reviews = [
+    {
+      id: 1,
+      name: "magdalena gessler",
+      job: "restaurateur",
+      img:
+        "https://mowic.pl/wp-content/uploads/2019/02/Gessler.jpg",
+      text:
+        "In my opinion this is restaurant with best pasta I ever eat!",
+    },
+    {
+      id: 2,
+      name: "adele",
+      job: "musician",
+      img:
+        "https://ocdn.eu/pulscms-transforms/1/TWOk9kpTURBXy9mZjUyNjU3OTExNTkwNzBkYmU5NWZhYTU0Yjc3MTM5Ny5qcGeTlQMAzQEGzQfQzQRlkwXNBLDNAqSTCaZjYmI1ZWUGgaEwAQ/adele.jpg",
+      text:
+        "I had one concert in Poland, but when I tried this steak dinner spieciality, I know I'll back here!",
+    },
+    {
+      id: 3,
+      name: "robert lewandowski",
+      job: "football player",
+      img:
+        "https://s3.tvp.pl/images2/3/5/b/uid_35baf125a2dc1b9ee59e8f1a34d94a7e1661771348258_width_1200_play_0_pos_0_gs_0_height_678_robert-lewandowski-fot-getty-images.jpg",
+      text:
+        "Me and my wife went there last evening and had house speciality,and it was the best sea food we ever tasted in our life. I give this restaurant a big five!",
+    },
+    {
+      id: 4,
+      name: "robert de niro",
+      job: "actor",
+      img:
+        "https://ocdn.eu/pulscms-transforms/1/illk9kpTURBXy9hMzc0NzlmYjdhMjE0ZDMwNGE5NGZjMTM1ZDI2OGUwOS5qcGeTlQMAMM0EAM0CQJMJpmZiM2QzMQaTBc0EsM0CdoGhMAE/robert-de-niro.jpg",
+      text:
+        "I visited a lot of countries looking for ideal burger. This is here!",
+    },
+  ];
